f6136aab42c018845da67e915811d067f868734b
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-04-24 16:44:32 +0300

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""Returns returns information about his/her TODO list progress with given  employee ID."""
 import requests
 import sys
 
