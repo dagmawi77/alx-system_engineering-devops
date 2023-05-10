f41c3feb20a69a40a3b24a4ec9b4d661a59bc9c4
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-05-10 15:01:42 +0300

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Using strace, find out why Apache is returning a 500 error.
+
+exec { 'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
