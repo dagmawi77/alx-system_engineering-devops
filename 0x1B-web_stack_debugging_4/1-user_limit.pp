acdb98a07eec2039e84e89388b288b405cc305a4
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-05-17 14:40:07 +0300

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,5 @@
 # Enable the user holberton to login and open files without error.
 
-# Increase hard file limit for Holberton user.
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
