735c934c227950a1afd3bf34cb4734ffa4bab3b8
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-08-09 12:07:19 +0300

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
-"""recursive function that queries the Reddit API,
-   """
+"""
+Defines a function that queries Reddit API
+"""
 import requests
-from sys import argv
-after = None
-count_dic = []
 
 
-def count_words(subreddit, word_list):
-    """function queries reddit api"""
-    global after
-    global count_dic
-    headers = {'User-Agent': 'xica369'}
-    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    response = requests.get(URL, headers=headers, allow_redirects=False,
-                            params=parameters)
+def count_words(subreddit, word_list, after=None, sort=True):
+    """
+    Queries the Reddit API, parses the title of all hot articles.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    params = {'after': after, 'limit': 100}
+    headers = {'User-Agent': 'advanced-api/0.0.1 by Mendy'}
+    req = requests.get(url=url,
+                       params=params, headers=headers, allow_redirects=False)
+    if req.status_code == 200:
+        response = req.json()
+        titles = [child['data']['title']
+                  for child in response['data']['children']]
+        after = response['data']['after']
+        if after is not None:
+            titles += count_words(subreddit,
+                                  word_list, after=after, sort=False)
+        if sort is True:
+            count = {k.lower(): 0 for k in word_list}
+            for title in titles:
+                count = {k: v + title.lower().split().count(k)
+                         for k, v in count.items()}
+            count = {k: v for k, v in count.items() if v > 0}
+            if len(count):
+                word_list = [w.lower() for w in word_list]
+                count = {k: v * word_list.count(k)
+                         for k, v in count.items()}
+                count = sorted(count.items(), key=lambda kv: (-kv[1], kv[0]))
+                [print("{}: {}".format(k, v)) for k, v in count]
+        else:
+            return titles
