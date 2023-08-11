18e50cf13c6bb68d58e53c6c3d85ed3f10fb1972
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-08-11 12:20:39 +0300

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,36 +3,40 @@
 Defines a function that queries Reddit API
 """
 import requests
+import json
 
+def count_words(subreddit, word_list):
+    if not word_list:
+        # Reached the end of the word list, print the results
+        print(*sorted(word_counts.items(), key=lambda x: (-x[1], x[0])), sep="\n")
+        return
 
-def count_words(subreddit, word_list, after=None, sort=True):
-    """
-    Queries the Reddit API, parses the title of all hot articles.
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    params = {'after': after, 'limit': 100}
-    headers = {'User-Agent': 'advanced-api/0.0.1 by Mendy'}
-    req = requests.get(url=url,
-                       params=params, headers=headers, allow_redirects=False)
-    if req.status_code == 200:
-        response = req.json()
-        titles = [child['data']['title']
-                  for child in response['data']['children']]
-        after = response['data']['after']
-        if after is not None:
-            titles += count_words(subreddit,
-                                  word_list, after=after, sort=False)
-        if sort is True:
-            count = {k.lower(): 0 for k in word_list}
-            for title in titles:
-                count = {k: v + title.lower().split().count(k)
-                         for k, v in count.items()}
-            count = {k: v for k, v in count.items() if v > 0}
-            if len(count):
-                word_list = [w.lower() for w in word_list]
-                count = {k: v * word_list.count(k)
-                         for k, v in count.items()}
-                count = sorted(count.items(), key=lambda kv: (-kv[1], kv[0]))
-                [print("{}: {}".format(k, v)) for k, v in count]
-        else:
-            return titles
+    # Get the hot articles for the subreddit
+    response = requests.get(f"https://api.reddit.com/r/{subreddit}/hot")
+    if response.status_code != 200:
+        # The subreddit is invalid, do nothing
+        return
+
+    # Parse the JSON response
+    articles = json.loads(response.content)
+
+    # Loop over the articles
+    for article in articles["data"]["children"]:
+        # Get the title of the article
+        title = article["data"]["title"].lower()
+
+        # Check if the title contains any of the keywords
+        for word in word_list:
+            if word in title:
+                # If it does, count the word
+                if word not in word_counts:
+                    word_counts[word] = 1
+                else:
+                    word_counts[word] += 1
+
+    # Recursively call the function with the remaining words
+    count_words(subreddit, word_list[1:])
+
+if __name__ == "__main__":
+    word_list = sys.argv[2].split()
+    count_words(sys.argv[1], word_list)
